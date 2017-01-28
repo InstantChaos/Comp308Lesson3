3acd7e6326d6edd8bac1678e988cfe75d64e91ae
Franco Chong <fnbeddie@hotmail.com>
2017-01-28 16:27:29 -0500

    Added Front-end dependencies

--- server.js
+++ server.js
@@ -1,35 +1,90 @@
-//My server will live here
+#!/usr/bin/env node
 
-let express = require('express');
+/**
+ * Module dependencies.
+ */
 
-//create an instance of the connect server
-let app = express();
+let app = require('./app');
+let debug = require('debug')('lesson3:server');
+let http = require('http');
 
-//represents the port
-const localport = 3000;
+/**
+ * Get port from environment and store in Express.
+ */
 
-/** * Get port from environment and store in Express. */
-let port = process.env.PORT || localport;
+let port = normalizePort(process.env.PORT || '3000');
 app.set('port', port);
 
+/**
+ * Create HTTP server.
+ */
 
-//listen for requests on a specific port
-app.listen(port);
-console.log(`Server listening at http://localhost/${port}`);
+let server = http.createServer(app);
 
-//Routing - mount different routes
+/**
+ * Listen on provided port, on all network interfaces.
+ */
 
-//Route for hello
-app.use('/hello',(req,res,next) =>{
-    res.setHeader('Content-Type', 'text/plain');
-    res.end("Hello World!");
-    next();
-});
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
 
-//test 4
-//Default route for website
-app.use('/',(req,res,next) =>{
-    res.setHeader('Content-Type', 'text/plain');
-    res.end("Welcome!");
-    next();
-});
+/**
+ * Normalize a port into a number, string, or false.
+ */
+
+function normalizePort(val) {
+  let port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
+
+/**
+ * Event listener for HTTP server "error" event.
+ */
+
+function onError(error) {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  let bind = typeof port === 'string'
+    ? 'Pipe ' + port
+    : 'Port ' + port;
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+
+function onListening() {
+  let addr = server.address();
+  let bind = typeof addr === 'string'
+    ? 'pipe ' + addr
+    : 'port ' + addr.port;
+  debug('Listening on ' + bind);
+}
